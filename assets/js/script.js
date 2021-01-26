b85a7687d95309f7c9cc368186ae33bf21df5882
Todd Malone <toddomalone@gmail.com>
2021-01-25 20:22:38 -0600

    added link to script.js, initial button click

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+// City Search:
+var $serachText = $("#city-serach");
+var $searchButton = $("#search-button");
+
+$("#search-button").on("click",function(event){
+    event.preventDefault()
+    console.log("clicked")
+});
