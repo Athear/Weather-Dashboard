9d5a4592159c8213bd84ca7884e6ea3986f55e96
Todd Malone <toddomalone@gmail.com>
2021-01-25 21:06:35 -0600

    buttons are now created with data attribute and layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,23 @@
 var $serachText = $("#city-serach");
 var $searchButton = $("#search-button");
 
+
+function buttonFactory(buttonVal){
+    newButton = $("<button>");
+    newButton.attr("data-city-name",buttonVal);
+    newButton.text(buttonVal);
+
+    //layout elements
+    newRow = $("<div class='row'>");
+    newCol = $("<div class='col-sm-12'>");
+
+    newCol.append(newButton);
+    newRow.append(newCol);
+    $(".search-log").append(newRow)
+}
+
 $("#search-button").on("click",function(event){
     event.preventDefault()
-    console.log("clicked")
+    console.log($("#city-serach").val())
+    buttonFactory($("#city-serach").val())
 });
